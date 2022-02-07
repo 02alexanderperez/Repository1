2f3407e789f97053d19d870362b85fa591115476
Alexander Perez <02alexander.perez@gulllakecs.org>
2022-02-07 14:16:54 -0700

    Import Time Library and Sleep

--- Welcome_InfoTechCenter.py
+++ Welcome_InfoTechCenter.py
@@ -1,6 +1,7 @@
+from time import sleep #Print to one line with time delay between prints
 # Code Name - Hornet
 
-print("Welcome to Hornets InfoTech Center\n")
-sleep(1)
+print("Welcome to Hornets InfoTech Center\n");sleep(1.5)
 print("Hornet's Operating System Booting Up")
-
+print("Initializing.")
+print("Initializing..")
