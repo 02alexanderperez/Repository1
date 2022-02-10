4818ed59512dcaa343f37678411c6aacf63668e9
Alexander Perez <02alexander.perez@gulllakecs.org>
2022-02-10 14:00:31 -0700

    Import Colorama to change text color
    Added color to text with colorama and askii

--- Welcome_InfoTechCenter.py
+++ Welcome_InfoTechCenter.py
@@ -1,7 +1,13 @@
-from time import sleep #Print to one line with time delay between prints
+from time import sleep#Print to one line with time delay between prints
+import colorama
+from colorama import Fore, Back, Style
+colorama.init(strip=False, autoreset=True)
+
+
 # Code Name - Hornet
 
-print("Welcome to Hornets InfoTech Center\n");sleep(1.5)
-print("Hornet's Operating System Booting Up")
-print("Initializing.")
-print("Initializing..")
+print("\033[1;34m Welcome To Hornets InfoTech Center! \n");sleep(1.5)
+print(Fore.WHITE + "Hornet's Operating System Booting Up")
+print(Fore.RED + "Initializing.");sleep(0.5)
+print(Fore.RED + " Initializing..");sleep(0.5)
+print(Fore.RED + "  Initializing...\n");sleep(0.5)
