52ca2688ce4de28b3ad85bdfdba92b3fec325b0b
Alexander Perez <02alexander.perez@gulllakecs.org>
2022-02-17 13:57:58 -0700

    Added randomnumbgen to calculate random distance
    used random.int to come up with a random number to print as our miles

--- Gas_InfoTechCenter.py
+++ Gas_InfoTechCenter.py
@@ -11,12 +11,13 @@ gasLevelIndicator = gasLevelGauge()
 #Create If-ELIF-ELSE statements using Comparative Operators to display gas level messages == In order to display messages
 def gasLevelAlert():
     gasStationList = ["Shell","BP","CircleK","Citgo","Mobile","Speedway","Marathon","Love's","Meijer","Costco","Sunoco",""]
+    randomnumbgen = (random.randint(1,25))
     if gasLevelIndicator == "Empty":
         print("******WARNING******\n ****No Gas****\n Calling Emergency Contact")
     elif gasLevelIndicator == "Low":
-        print("**WARNING**\n *Low Gas*\n Nearest Gas Station Is" + random.choice(gasStationList))
+        print("**WARNING**\n *Low Gas*\n Nearest Gas Station Is " + random.choice(gasStationList) + " Which is " + str(randomnumbgen) + " Miles Away")
     elif gasLevelIndicator == "Quarter Tank":
-        print("Quarter Tank Of Gas Remaining\n Recommend Refueling")
+        print("Quarter Tank Of Gas Remaining\n Nearest gas station is " + random.choice(gasStationList) + " Which is " + str(randomnumbgen) + " Miles Away")
     elif gasLevelIndicator == "Half Tank":
         print("Half A tank Of Gas Remaining\n 150 miles remaining")
     elif gasLevelIndicator == "Three Quarter Tank":
